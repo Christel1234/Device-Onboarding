a863611148e50d36fc0d976ec32f66f4745e9a50
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-02-25 12:17:43 +0200

    Created mockdatabase, mockdevice and test file

---
+++ mockdevice.py
@@ -0,0 +1,9 @@
+class mockdevice:
+    def __init__(self, serialID, boxID, crateID, isDamaged):
+        self.serialID = serialID
+        self.boxID = boxID
+        self.crateID = crateID
+        self.isDamaged = isDamaged
+        
+
+    
---
+++ testDeviceOnboarding.py
@@ -0,0 +1,9 @@
+import unittest
+from mockdatabase import *
+from mockdevice import *
+
+class testDeviceOnboarding(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
