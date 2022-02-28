9e88042dea5a6080d78a6d382a0d7d629e09d744
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-02-28 13:21:33 +0200

    set up warehouse constructor

---
+++ warehouse.py
@@ -0,0 +1,8 @@
+class warehouse:
+    def __init__(self,warehouse_number,section_number,row_number,shelf_number,segment_number,segment_section):
+        self.warehouse_number = warehouse_number
+        self.section_number = section_number
+        self.row_number = row_number
+        self.shelf_number = shelf_number
+        self.segment_number = segment_number
+        self.segment_section = segment_section
