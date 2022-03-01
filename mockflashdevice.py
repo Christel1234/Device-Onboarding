706b27372d06a530df1b1172214cea43acc9e74d
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-03-01 16:26:34 +0200

    mockflashdevice class

--- mockflashdevice.py
+++ mockflashdevice.py
@@ -1,2 +1,17 @@
-class mockflashdevice:
-    pass
+from device import *
+from errorhandler import FlashFaliureException
+
+class Iflashdevice:
+    def flashdevice(self):
+        pass
+    
+class flashdevice(Iflashdevice):
+    def __init__(self, device: device):
+        self.device = device
+
+    def flashdevice(self):
+        try:
+            self.device.flashdevice()
+        except:
+            raise FlashFaliureException
+
