a75592aba7690c9f7621b4daeceac0a679484e72
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-03-01 13:56:22 +0200

    mockkeyinjection class

--- mockkeyinjection.py
+++ mockkeyinjection.py
@@ -1,2 +1,17 @@
-class mockkeyinjection:
-    pass 
+from device import *
+from errorhandler import InjectionFaliureException
+
+class Ikeyinjection:
+    def keyinjection(self):
+        pass
+
+class keyinjection(Ikeyinjection):
+    def __init__(self, key):
+        self.key = key
+
+    def injectkey(self, key):
+        try:
+            self.injectkey(key)
+            return True
+        except:
+            raise InjectionFaliureException
