4969683aea1e4e2c326aa4106ef8bf6cdea770b8
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-03-01 18:39:25 +0200

    accessors added

--- sim.py
+++ sim.py
@@ -2,4 +2,7 @@ class sim:
     def __init__(self,SNN,IMSI):
         self.SNN = SNN 
         self.IMSI = IMSI
-
+    def get_SNN(self):
+        return self.SNN
+    def get_IMSI(self): 
+        return self.IMSI
