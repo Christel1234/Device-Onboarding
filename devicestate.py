7d14d26dd1500045d5106022a0427588ba699a8e
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-03-01 16:30:04 +0200

    some order changes

--- devicestate.py
+++ devicestate.py
@@ -1,13 +1,13 @@
 import enum
 
 class devicestate(enum.Enum):
-    device_received = 1
+    new_device = 1
     serialID_recorded = 2
-    package_recorded = 3
-    damage_recorded = 4
-    simcard_assigned = 5
-    imei_recorded = 6
+    imei_recorded = 3
+    packageID_recorded = 4
+    damage_recorded = 5
+    simcard_assigned = 6
     device_flashed = 7
     keys_injected = 8
-    sent_for_repacking = 9
-    stored_in_warehouse = 10
+    repacking = 9
+    warehouse = 10
