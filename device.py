06c2fe75749468ac2c270e94760c8015cd6f884f
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-03-04 12:21:55 +0200

    some added setters

--- device.py
+++ device.py
@@ -4,33 +4,38 @@ from mockkeyinjection import *
 from devicestate import *
 
 class device:
-    def __init__(self, serialID, packageID, isDamaged, flashed, keyinjected, sendrepacking, IMEI):
+    def __init__(self, serialID, box, crate, isDamaged, flashed, keyinjected, sendrepacking, IMEI):
         self.serialID = serialID
-        self.packageID = packageID
+        self.box = box
+        self.crate = crate
         self.isDamaged = isDamaged
         self.flashed = flashed 
         self.keyinjected = keyinjected
         self.sendrepacking = sendrepacking 
         self.IMEI = IMEI
-        
-#update state
+
+    def set_serialID(self,serialID):
+        self.serialID = serialID
+        self.state = devicestate.serialID_recorded
     def get_serialID(self):
         return self.serialID
 
-    def set_packageID(self,box,crate):
+    def set_box(self,box):
         self.box = box
-        self.crate = crate 
-        self.state = devicestate.packageID_recorded
+        self.state = devicestate.box_recorded
         return True
     def get_box(self):
         return self.box
+
+    def set_crate(self,crate):
+        self.crate = crate
+        self.state = devicestate.crate_recorded
     def get_crate(self):
         return self.crate
 
-##figure out this one - maybe create method
-    def set_isDamaged(self):
-        if self.isDamaged:
-            self.set_device_state("damage_recorded")
+    def set_isDamaged(self,isDamaged):
+        self.state = devicestate.damage_recorded(isDamaged)
+        if self.state == 0:
             self.flashed = None
             self.keyinjected = None
             self.needrepacking = None
@@ -38,16 +43,21 @@ class device:
     def get_isDamaged(self):
         return self.isDamaged
 
-
-##need flash and inject method and update state
+    def set_flashed(self,flashed):
+        self.flashed = flashed
+        self.state = devicestate.device_flashed
     def get_flashed(self):
         return self.flashed
 
+    def set_keyinjected(self,keyinjected):
+        self.keyinjected = keyinjected
+        self.state = devicestate.key_injected
     def get_keyinjected(self):
         return self.keyinjected
 
-
-#need to update state
+    def set_sendrepacking(self,sendrepacking):
+        self.sendrepacking = sendrepacking
+        self.state = devicestate.repacking_sent
     def get_sendrepacking(self):
         return self.needrepacking
 
@@ -56,35 +66,24 @@ class device:
         self.state = devicestate.imei_recorded
     def get_IMEI(self):
         return self.IMEI
-##is this necessarry?
-    def set_device_state(self,state): 
-        self.devicestate = state
-    def get_device_state(self):
-        return self.devicestate
 
-#update state to assign simcard
     def set_sim_info(self,SNN,IMSI):
         self.SNN = SNN
         self.IMSI = IMSI
+        self.state = devicestate.simcard_assigned
     def get_sim_snn(self):
         return self.SNN
     def get_sim_imsi(self):
         return self.IMSI
 
-#check this
-    #def legal(self, wanted_state):
-        #check if next state update is correct
-
-"""needs changing
-
-    def set_warehouse_info(self,warehouse):
-
+    def set_warehouse_info(self,warehouse_number,section_number,row_number,shelf_number,segment_number,segment_section):
         self.warehouse_number = warehouse_number
         self.section_number = section_number
         self.row_number = row_number
         self.shelf_number = shelf_number
         self.segment_number = segment_number
         self.segment_section = segment_section
+        self.state = devicestate.warehouse_stored
     def get_warehouse_number(self):
         return self.warehouse_number
     def get_section_number(self):
@@ -97,8 +96,12 @@ class device:
         return self.segment_number
     def get_segment_section(self):
         return self.segment_section
-"""
     
-#newdevice = device(1,False,False,False,False,1)
-#newdevice.set_sim_info(1,1)
-#newdevice.set_warehouse_info(1,2,3,4,5,'BACK RIGHT')
+    def set_device_state(self,state): 
+        self.devicestate = state
+    def get_device_state(self):
+        return self.devicestate
+
+newdevice = device(1,1,1,False,False,False,False,1)
+newdevice.set_sim_info(1,1)
+newdevice.set_warehouse_info(1,2,3,4,5,'BACK RIGHT')
